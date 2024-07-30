6a1173e2f35060000e63c7650c0468ed29af00ca
Emily Guo <emilyguo@amazon.com>
2024-07-30 14:28:50 -0700

    trying to server route

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,4 +1,5 @@
 import { IRouter } from '../../../../src/core/server';
+import { schema } from '@osd/config-schema';
 
 export function defineRoutes(router: IRouter) {
   router.get(
@@ -10,6 +11,23 @@ export function defineRoutes(router: IRouter) {
       return response.ok({
         body: {
           time: new Date().toISOString(),
+        },
+      });
+    }
+  );
+  router.get(
+    {
+      path: '/_insights/top_queries',
+      validate: {
+        query: schema.object({}),
+      },
+    },
+    async (context, request, response) => {
+      return response.custom({
+        statusCode: 200,
+        body: {
+          ok: true,
+          response: request.query,
         },
       });
     }
